655561fef5ba7e6b40395021866d65b1f8ffc132
Max Amsler <max.amsler@northwestern.edu>
2016-04-05 19:07:41 -0500

    Added abinit.py

---
+++ dfttopif/parsers/abinit.py
@@ -0,0 +1,15 @@
+from .base import DFTParser
+import os
+from ase.calculators.vasp import VASP
+
+class VaspParser(DFTParser):
+    """
+    Parser for VASP calculations
+    """
+    
+    def get_name(): return "VASP"
+    
+    def test_if_from(self, directory):
+        # Check whether it has an INCAR file
+        return os.path.isfile(os.path.join(directory, 'INCAR'))
+        
