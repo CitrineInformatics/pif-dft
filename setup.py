f90161fe192388c58c9d50964b6e2fbab93df6a8
Max Hutchinson <maxhutch@gmail.com>
2017-02-01 09:34:56 -0600

    Used the wrong password; fixed

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='dfttopif',
-    version='0.1.5',
+    version='0.1.6',
     description='Library for parsing Density Functional Theory calculations',
-    long_description=readme,
     url='https://github.com/CitrineInformatics/pif-dft',
-    license=license,
     install_requires=[
         'ase',
         'pypif',
