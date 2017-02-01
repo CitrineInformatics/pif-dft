2bbff1e2e95357180a9d12f41b80fc2e70977518
Max Hutchinson <maxhutch@gmail.com>
2017-02-01 08:34:24 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
     name='dfttopif',
-    version='0.1.1',
+    version='0.1.4',
     description='Library for parsing Density Functional Theory calculations',
     long_description=readme,
     url='https://github.com/CitrineInformatics/pif-dft',
