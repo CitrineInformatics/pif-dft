b585fe6fa2015cd23b051b40e66d6c22a7794029
Max Hutchinson <maxhutch@gmail.com>
2017-02-07 07:34:32 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfttopif',
-    version='0.1.6',
+    version='0.1.7',
     description='Library for parsing Density Functional Theory calculations',
     url='https://github.com/CitrineInformatics/pif-dft',
     install_requires=[
