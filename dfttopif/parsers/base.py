d1347d6f58866e3e68a0505c11c21715469bfdf7
sbajaj <saurabhbajaj2@gmail.com>
2018-01-10 10:58:57 -0800

    minor bug fix -> add stress function to results

--- dfttopif/parsers/base.py
+++ dfttopif/parsers/base.py
@@ -109,6 +109,7 @@ class DFTParser(object):
             'Density': 'get_density',
             'OUTCAR': 'get_outcar',
             'Total magnetization': 'get_total_magnetization',
+            'Stresses': 'get_stresses'
         }
         
     def _call_ase(self, func):
